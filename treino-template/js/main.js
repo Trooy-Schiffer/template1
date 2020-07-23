c7bcb5441dd54f415478fa78f771d77215a2beaf
Thiago Souza <68604314+Trooy-Schiffer@users.noreply.github.com>
2020-07-22 21:12:37 -0400

    template

---
+++ treino-template/js/main.js
@@ -0,0 +1,9 @@
+$('.slide_principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 5000
+});
